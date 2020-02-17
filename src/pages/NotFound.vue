3bff03fff49dff256d4a7be5a89072699ffa5ddd
Timothy Choi <turokchoi@gmail.com>
2020-02-16 18:20:46 -0800

    feat: page not found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="col-full">
-    <h1> Not Found</h1>
-    <p>gtfo </p>
+    <h1 :class="$style.headingLarge"> Not Found</h1>
+    <p :class="$style.textLarge"> we couldn't find what you are looking for </p>
+    <router-link :to="{name: 'Hello'}"> Go HOME </router-link>
   </div>
 </template>
+
+<style module>
+  .headingLarge {
+    font-size: 100px;
+  }
+  .textLarge {
+    font-size: 50px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home'
 import ThreadShow from '@/pages/ThreadShow'
+import NotFound from '@/pages/NotFound'
 
 Vue.use(Router)
 
@@ -21,7 +22,7 @@ export default new Router({
     {
       path: '*',
       name: 'Not Found',
-      redirect: {name: 'Home'}
+      component: NotFound
     }
   ],
   mode: 'history'
